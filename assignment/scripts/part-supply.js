7b971c51325a568a0cfd7cca3d83398930cbefae
Danny Mussie <dannymussie7@gmail.com>
2021-07-29 10:45:33 -0500

    completed parts 3 & 4

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -14,10 +14,13 @@ let supplyChanges = [3, 5, -6, 0, 7, 11];
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+console.log(supplyChanges[1]);
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges'
 //    array & console.log the value removed.
 console.log('4. Removed item:');
+let removedItem = supplyChanges.pop();
+console.log(removedItem);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
